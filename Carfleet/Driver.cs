4ecd41f133a68a0219722178938813cf304d2094
Logan Nguyen <logan.nguyen@eduvaud.ch>
2023-11-22 09:28:04 +0100

    added new class

--- Carfleet/Driver.cs
+++ Carfleet/Driver.cs
@@ -1,21 +1,12 @@
-﻿namespace Carfleet
-{
-    public class Driver
-    {
-        #region private attributes
-        private string name;
-        private string firstname;
-        private string city;
-        private string phonenumber;
-        private string emailaddress;
-        private string language;
-        private string workZone;
-        #endregion private attributes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        #region public methods
-        public Driver(string name, string firstname, string phonenumber, string emailaddress, string language = "", string workZone = "")
-        {
-        }
-        #endregion public methods
+namespace Carfleet
+{
+    internal class Driver
+    {
     }
 }
